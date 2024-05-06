ef525a15c25d012e6c3150c1a2cd9685dab03df4
OLIWIERTE21C <oliwier.kondratowicz@elev.ga.ntig.se>
2024-05-06 10:25:16 +0200

    yahhooo

--- src/NetworkingServer.java
+++ src/NetworkingServer.java
@@ -9,11 +9,13 @@ public class NetworkingServer {
         ServerSocket server = null;
         Socket client;
 
+        //Default port number we are going to use
         int portnummer = 4444;
         if (args.length>=1){
             portnummer = Integer.parseInt(args[0]);
         }
 
+        //Create Server side socket
         try{server=new ServerSocket(portnummer);
         } catch (IOException ie) {
             System.out.println("Cannot open socket." + ie);
@@ -21,8 +23,12 @@ public class NetworkingServer {
         }
         System.out.println("ServerSocket is created " + server);
 
+        //Wait for the data from the client and reply
         while (true) {
             try {
+                //Listens for a connection to be made to
+                //this socket and accepts it. The method blocks until
+                //a connection is made
                 System.out.println("Waiting for connect request...");
                 client = server.accept();
 
@@ -31,11 +37,13 @@ public class NetworkingServer {
                 int clientPort = client.getPort();
                 System.out.println("Client Host = " + clientHost + "Client Port = " + clientPort);
 
+                //Read data from the client
                 InputStream clientIn = client.getInputStream();
                 BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
                 String msgFromClient = br.readLine();
                 System.out.println("Message received from client = " + msgFromClient);
 
+                //Send response to the client
                 if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
                     OutputStream clientOut = client.getOutputStream();
                     PrintWriter pw = new PrintWriter(clientOut, true);
@@ -43,6 +51,7 @@ public class NetworkingServer {
                     pw.println(ansMsg);
                 }
 
+                //Close sockets
                 if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
                     server.close();
 
@@ -50,7 +59,7 @@ public class NetworkingServer {
                     break;
                 }
             } catch (IOException ie){
-                System.out.println("Error");
+                System.out.println("Error, didn't close");
             }
         }
     }
